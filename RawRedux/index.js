6f3acc08fbeee3ae15e505d73c7c9d5d40415314
amitdubey <amitdubey6261@gmail.com>
2022-11-18 19:45:09 +0530

    readMiddlewareFileAndUnderStandLogger

--- RawRedux/index.js
+++ RawRedux/index.js
@@ -9,8 +9,11 @@ cakes are in shelf and icecreams are on freezer thus
 2. shopkeeper-2 handle freezer
 */
 const redux = require('redux');
+const reduxLogger = require('redux-logger')
 const createStore = redux.createStore ;
 const combineReducers = redux.combineReducers // TO use this method we have create a root redcer and use taht reduvcer in our our code
+const logger = reduxLogger.createLogger() // --------------> will use as a middleware thus library provide a function called redux middleware
+const applyMiddleware = redux.applyMiddleware // -------> to apply this pass it as an argument to store with middleawre as an argument
 
 
 
@@ -86,7 +89,7 @@ const iceceamReducer = (state = intialIceCreamState , action ) =>{
     }
 }
 
-//COMBINE REDUCER KA ROOT REDUCER
+//COMBINE REDUCERs IN ROOT REDUCER
 
 const rootReducer = combineReducers({
     cake : cakeReducer , 
@@ -94,9 +97,9 @@ const rootReducer = combineReducers({
 })
 
 // const store = createStore(reducer) //Responibility -1 Holding the application state      ------------> ONLY ACCEPTS SINGLE REDUCER THUS WE have to use COMBINE REDUCER METHOD SUch that we can use icecreamreducer and cakereducer simultaneously
-const store = createStore(rootReducer);
+const store = createStore(rootReducer , applyMiddleware(logger));
 console.log('InitialState' , store.getState() ); //Responisibility-2 Allow access via getState method by which we can see current state
-const unsubscribe = store.subscribe(()=>{console.log(`Updated Store`,store.getState())}) //Responibility-3 Allow to listen any changes that has happened to the store current state
+const unsubscribe = store.subscribe(()=>{/*console.log(`Updated Store`,store.getState())*/}) //Responibility-3 Allow to listen any changes that has happened to the store current state ---------------> now not require to log anything we have logger
 store.dispatch(buyCake())
 store.dispatch(buyIcecream())
 store.dispatch(buyCake())
