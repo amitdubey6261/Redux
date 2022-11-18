5ca3ae1b62efab73f8955444aeacfaf1dbbf34c8
amitdubey <amitdubey6261@gmail.com>
2022-11-18 18:02:37 +0530

    actionAndReducers

--- RawRedux/index.js
+++ RawRedux/index.js
@@ -1,1 +1,32 @@
-console.log("ok");
+const BUY_CAKE = 'BUY_CAKE'
+
+//Action :
+// let action = {
+//     type : BUY_CAKE , 
+//     info : "First redux action"
+// }
+
+//Action Creator
+
+let buyCake = () =>{
+    return {
+        type : BUY_CAKE , 
+        info : 'First Redux Action '
+    }
+}
+
+const initialState = {
+    numOfCakes : 10
+}
+
+const reducer = (state = initialState , action)=>{
+    switch(action.type){
+        case BUY_CAKE : return{
+            ...state , 
+            numOfCakes : state.numOfCakes-1
+        }
+        default : return state
+    }
+}
+
+
